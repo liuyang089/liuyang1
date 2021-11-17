01490363573429e3ebca1f9327a7e6be4eded690
liuyang <a1232007s@163.com>
2021-11-17 11:52:06 +0800

    dsdssdxzx

--- lagou-parent/lagou-cloud-config/src/main/java/com/lagou/config/aas.java
+++ lagou-parent/lagou-cloud-config/src/main/java/com/lagou/config/aas.java
@@ -4,5 +4,6 @@ public class aas {
     public static void main(String[] args) {
         System.out.println("dsdsd");
         System.out.println("sdsd");
+        System.out.println("adsd");
     }
 }
