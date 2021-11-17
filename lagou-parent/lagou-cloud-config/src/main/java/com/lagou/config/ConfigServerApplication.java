3e05ae475e6d82c387d1035f89650402a496eac7
liuyang089 <90133384+liuyang089@users.noreply.github.com>
2021-11-17 12:11:30 +0800

    Update ConfigServerApplication.java

--- lagou-parent/lagou-cloud-config/src/main/java/com/lagou/config/ConfigServerApplication.java
+++ lagou-parent/lagou-cloud-config/src/main/java/com/lagou/config/ConfigServerApplication.java
@@ -12,5 +12,9 @@ public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class,args);
          System.out.println("124");
+          System.out.println("2");
+        System.out.println("2");
+        System.out.println("2");
+        System.out.println("2");
     }
 }
